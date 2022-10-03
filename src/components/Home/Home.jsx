c18c20226a47d4e00b040334d435b19531c55cde
FrontEndDevo <mylinkacco@gmail.com>
2022-10-03 11:47:40 +0200

    Finish img and titles in Home component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,5 @@
 import classes from "./Home.module.scss";
+import { init } from "ityped";
 import photo from "../../assets/me/Linked in.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -6,19 +7,30 @@ import {
   faGithub,
   faFacebook,
 } from "@fortawesome/free-brands-svg-icons";
+import { useEffect, useRef } from "react";
 
 const Home = () => {
+  // That's all about iTyped.js package, it adds nice animation.
+  const textRef = useRef();
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      startDelay: 500,
+      backDelay: 1000,
+      strings: ["Front-End","Web Developer"],
+    });
+  }, []);
+
   return (
     <div className={classes.home}>
-      <div>
+      <div className={classes["developer-photo"]}>
         <img src={photo} alt="developer" />
       </div>
       <div className={classes["developer-details"]}>
         <div className={classes["developer-info"]}>
           <h3>Hi There, I'm</h3>
           <h2>Beshoy Tag</h2>
-          <h4>
-            Front-End <span>Web Developer</span>
+          <h4><span ref={textRef}></span>
           </h4>
         </div>
         <div className={classes["developer-contacts"]}>
