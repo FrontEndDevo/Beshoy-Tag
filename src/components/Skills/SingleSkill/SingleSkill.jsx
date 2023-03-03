332035e96567fbf1ad78d4cad3303a4d4343a118
FrontEndDevo <mylinkacco@gmail.com>
2023-03-03 20:34:13 +0200

    Add dark-mode to SingleSkill component

--- src/components/Skills/SingleSkill/SingleSkill.jsx
+++ src/components/Skills/SingleSkill/SingleSkill.jsx
@@ -1,8 +1,16 @@
 import classes from "./SingleSkill.module.scss";
+import { useContext } from "react";
+import DarkMoodContext from "../../../store/darkMood-context";
 
 const SingleSkill = (props) => {
+  const darkMode = useContext(DarkMoodContext);
+
   return (
-    <li className={classes["tech-box"]}>
+    <li
+      className={`${classes["tech-box"]} ${
+        darkMode.isDarkMoodOn && classes["dark-mode"]
+      }`}
+    >
       <img src={props.icon} alt={`${props.title}-icon`} />
       <span>{props.title}</span>
     </li>
