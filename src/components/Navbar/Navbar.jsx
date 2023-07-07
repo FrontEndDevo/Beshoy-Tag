5eeb8c4d70f3dc0fe8d4462aff84424d8973205b
FrontEndDevo <mylinkacco@gmail.com>
2023-07-08 01:17:32 +0300

    Add menu div with three span in Navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -17,31 +17,40 @@ const Navbar = () => {
     },
   };
 
+  const navLinks = (
+    <ul>
+      <li>
+        <NavLink
+          activeClassName={classes["class-active"]}
+          to="/Beshoy-Tag/home"
+        >
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/Beshoy-Tag/portfolio">Portfolio</NavLink>
+      </li>
+      <li>
+        <NavLink to="/Beshoy-Tag/about">About</NavLink>
+      </li>
+      <li>
+        <NavLink to="/Beshoy-Tag/skills">Skills</NavLink>
+      </li>
+    </ul>
+  );
+
   return (
     <nav className={classes.navbar}>
       <div className={classes.title}>
         <h1>Beshoy Tag</h1>
       </div>
       <div className={classes["nav-links"]}>
-        <ul>
-          <li>
-            <NavLink
-              activeClassName={classes["class-active"]}
-              to="/Beshoy-Tag/home"
-            >
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/Beshoy-Tag/portfolio">Portfolio</NavLink>
-          </li>
-          <li>
-            <NavLink to="/Beshoy-Tag/about">About</NavLink>
-          </li>
-          <li>
-            <NavLink to="/Beshoy-Tag/skills">Skills</NavLink>
-          </li>
-        </ul>
+        <div className={classes.menu}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        {navLinks}
       </div>
       <div className={classes.sunmoon} onClick={darkModeCtx.toggleDarkMode}>
         <Lottie
